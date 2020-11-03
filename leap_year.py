efee33c98d32b9ee963fb3bc1210fbd54a254e26
Jansenalex <70344081+Jansenalex5@users.noreply.github.com>
2020-11-03 15:21:05 -0500

    Create leap_year.py
    Finished Leap Year assignment

---
+++ leap_year.py
@@ -0,0 +1,30 @@
+def leap_year(year):
+    """
+    - Add code in the defined function to figure out whether or not the given year is a leap year or not. 
+    
+    - Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial years are leap years if they are exactly divisible by 400. For example, the years 1700, 1800, and 1900 are not leap years, but the years 1600 and 2000 are. - Wikipedia
+    
+    - Take in a parameter called year and return “Is a leap year” or “Not a leap year”
+    """
+    # Write your code here. 
+
+    if(year%4)==0:
+        if(year%100)==0:
+            if(year%400)==0:
+                print("{0} is a leap year".format(year))
+            else:
+                print("{0} is not a leap year".format(year))
+        else:
+            print("{0} is a leap year".format(year))
+    else:
+        print("{0} is not a leap year".format(year))
+
+
+
+if __name__ == "__main__":
+    years = [2000, 1999, 1912, 3002, 1700, 1400]
+    answers = []
+    for year in years:
+        answers.append(leap_year(year))
+    
+  
