5ff821dda5037503dc127b4849086ef633169144
Jansenalex <70344081+Jansenalex5@users.noreply.github.com>
2020-11-09 17:24:56 -0500

    Create emoji.py

---
+++ emoji.py
@@ -0,0 +1,30 @@
+# import emoji module 
+import emoji 
+
+# grinning face 
+print("\U0001f600") 
+
+# grinning squinting face 
+print("\U0001F606") 
+
+# rolling on the floor laughing 
+print("\U0001F923") 
+
+#Wink
+print("hello","\N{winking face}")
+
+#Thumbs up
+print("\N{thumbs up sign}")
+
+#Kissy Face 
+print("\U0001F61A")
+
+#Cowboy Hat
+print("\U0001F920")
+
+print("\U0001F415 DOG")
+
+
+
+
+
